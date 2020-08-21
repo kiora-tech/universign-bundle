4b0925cb607d1c7a2a23fb73da4b6559f5a4091d
Rathgeber Stéphane <stephane.kiora@gmail.com>
2020-08-21 17:55:44 +0200

    fix service declation

--- DependencyInjection/KioraUniversignExtension.php
+++ DependencyInjection/KioraUniversignExtension.php
@@ -2,6 +2,7 @@
 
 namespace Kiora\UniversignBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -13,5 +14,13 @@ class KioraUniversignExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $configuration = new Configuration();
+        $processor = new Processor();
+        $config = $processor->processConfiguration($configuration, $configs);
+
+        $container->setParameter('kiora_universign.path', $config['path']);
+        $container->setParameter('kiora_universign.user', $config['user']);
+        $container->setParameter('kiora_universign.password', $config['password']);
     }
 }
